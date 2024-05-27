b52be55bde456e2a4809fd215bc82aee2e68e90d
ductt7575 <duc28072003@gmail.com>
2024-05-27 20:38:47 +0700

    import react-slick

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import AppHeader from '@/components/header/app.header';
+import MainSlider from '@/components/main/main.slider';
+import { Container } from '@mui/material';
 
 export default function HomePage() {
   return (
     <>
-      <div>Home Page</div>
+      <Container>
+        <MainSlider />
+      </Container>
     </>
   );
 }
